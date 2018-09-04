2543157c28bf34d64a3be66d9fc217c39de12b0a
Priyank Kumar <pkumar3@aptean.com>
2018-09-04 13:54:23 +0530

    added the project

---
+++ MyMuzic/MyMuzic/Models/SearchSong.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMuzic.Models
+{
+    public class SearchSong
+    {
+        public Guid Id { get; set; }
+        public string Keyword { get; set; }
+
+    }
+}
